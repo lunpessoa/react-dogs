92215170bd1b2f1a6823db09deed1b5ce3fdc1a0
Luan <luan.pessoa.393@hotmail.com>
2021-04-06 03:41:44 -0300

    LastAdjustmentsDesign

--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -37,7 +37,7 @@ const LoginPasswordReset = () => {
   }
 
   return (
-    <div>
+    <section className="animeLeft">
       <Head title="Reset a senha"/>
       <h1 className="title">Resete a Senha</h1>
       <form onSubmit={handleSubmit}>
@@ -45,7 +45,7 @@ const LoginPasswordReset = () => {
         {loading ? <Button disabled>Resetando...</Button> : <Button>Resetar</Button>}
         <Error error={error} />
       </form>
-    </div>
+    </section>
   )
 }
 
