cb00bebfc7d98afe01b4b3d45f41b3e3d28088a3
Luan <luan.pessoa.393@hotmail.com>
2021-04-05 23:54:54 -0300

    FeedImages

---
+++ src/Components/Photo/PhotoContent.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import PhotoComments from './PhotoComments';
+import styles from './PhotoContent.module.css'
+import PhotoDelete from './PhotoDelete';
+import { UserContext } from '../../UserContext';
+
+const PhotoContent = ({data}) => {
+  const {photo, comments} = data;
+  const user = React.useContext(UserContext);
+  
+  return (
+    <div className={styles.photo}>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title} />
+      </div>
+      <div className={styles.details}>
+        <p className={styles.author}>
+          {user.data && user.data.username === photo.author ? (
+            <PhotoDelete id={photo.id}/> 
+          ) : (
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          )}
+          <span className={styles.visualizacoes}>{photo.acessos}</span>
+        </p>
+        <h1 className="title">
+          <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+        </h1>
+        <ul className={styles.attributes}>
+          <li>{photo.peso} kg</li>
+          <li>{photo.idade} anos</li>
+        </ul>
+      </div>
+      <PhotoComments id={photo.id} comments={comments} />
+    </div>
+  )
+}
+
+export default PhotoContent
